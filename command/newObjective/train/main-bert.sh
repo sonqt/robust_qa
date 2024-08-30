4b004d4b6828fcd0fa8d9762dd5fd2987bda84d4
sonqt <sqt2@jacqueline.infosci.cornell.edu>
2024-08-30 09:45:04 -0400

    rename main code file

--- command/newObjective/train/main-bert.sh
+++ command/newObjective/train/main-bert.sh
@@ -1,25 +1,32 @@
-run_qa="/Volumes/Share/tran_s2/SQuAD2_new/code/run_squad2_multi.py"
-trainObjective="SQuADv2-multians"
-saveModelPath="/Volumes/Share/tran_s2/SQuAD2_new/Models"
-model="bert-base-cased"
-for seed in 1 2 3 4 5
-do
-    python ${run_qa} \
-        --model_name_or_path ${model} \
-        --train_file "/Volumes/Share/tran_s2/SQuAD2_new/data/train-squad-v2-multians.json" \
-        --validation_file "/Volumes/Share/tran_s2/Public_Datasets/SQuAD/squad2/dev-v2.0-new-format.json" \
-        --do_train \
-        --do_eval \
-        --per_device_train_batch_size 16 \
-        --per_device_eval_batch_size 8 \
-        --learning_rate 2e-5 \
-        --num_train_epochs 3 \
-        --max_seq_length 384 \
-        --max_answer_length 128 \
-        --doc_stride 128 \
-        --seed ${seed} \
-        --save_steps 0.1 \
-        --overwrite_output_dir \
-        --version_2_with_negative \
-        --output_dir  "${saveModelPath}/${trainObjective}/${model}/seed-${seed}"
+#new_code == train-squad-agent
+run_qa="/home/sqt2/myExperiment/robust_qa/code/run_squad2_multi.py"
+trainObjective="New-AGent"
+saveModelPath="/reef/sqt2/QA_New_Method/Test_Ablation"
+for model in "bert-base-cased" 
+do 
+    qa_lambda=2
+    tagging_lambda=1
+    for seed in 1 
+    do
+        CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=5 python ${run_qa} \
+            --model_name_or_path ${model} \
+            --qa_lambda ${qa_lambda} \
+            --tagging_lambda ${tagging_lambda} \
+            --train_file "/home/sqt2/myExperiment/New_robust_qa/data/training/train-squad-agent.json" \
+            --validation_file "/home/sqt2/myExperiment/New_robust_qa/data/dev/SQuAD/dev-v2.0.json" \
+            --do_train \
+            --do_eval \
+            --per_device_train_batch_size 16 \
+            --per_device_eval_batch_size 8 \
+            --learning_rate 2e-5 \
+            --num_train_epochs 3 \
+            --max_seq_length 384 \
+            --max_answer_length 128 \
+            --doc_stride 128 \
+            --seed ${seed} \
+            --save_steps 999999999 \
+            --overwrite_output_dir \
+            --version_2_with_negative \
+            --output_dir  "${saveModelPath}/${trainObjective}/${model}/seed-${seed}"
+    done
 done
